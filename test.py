01c07f405305773cc0e4f622dcd21bf7826b77bb
Your Name <you@example.com>
2018-10-04 22:26:08 +0800

    fixed

--- test.py
+++ test.py
@@ -48,7 +48,7 @@ def rs485_test():
 
 	ser = serial.Serial('/dev/ttyUSB0',9600,timeout=23)  # open serial port
 	print(ser.name)         # check which port was really used
-	ser.write(b'\n\rif you see this,\n\r please enter [ exit ] to return!"')
+	ser.write(b'\n\rif you see from remote machine,\n\rplease enter [ exit ] to return!"')
 	state = ser.read(4)
 	#print(state)
 	
