ca382687bf20fd7504eaefaaba030b664d7fa543
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-19 18:02:50 +0800

    modufy

--- ADC3a.py
+++ ADC3a.py
@@ -2,11 +2,10 @@
 
 import smbus
 import time
-from py7seg import Py7Seg # xxx
 
 bus = smbus.SMBus(1) # RPi revision 2 (0 for revision 1)
 i2c_address = 0x4D  # default address
-ps = Py7Seg() # xxx
+
 t = 0
 while True:
     # Reads word (2 bytes) as int
@@ -16,7 +15,7 @@ while True:
     # Ignores two least significiant bits
     data = data >> 2
     print("T={%s}"%data) 
-    ps.showText("%4d" %data) # xxx
+
     t += 0.1
     time.sleep(0.1)
     
