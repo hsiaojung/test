7cf99000958f0d6d0a6602b7a2c27f661a599237
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-19 15:44:41 +0800

    1

--- test.py
+++ test.py
@@ -45,14 +45,18 @@ def lora_tx():
 
 	ser = serial.Serial('/dev/ttyAMA0',115200,timeout=23)  # open serial port
 	print("You need to set RX of LoRa fisrt \n\n")         # check which port was really used
+	ser.write(b'\r\n')
+	
 	ser.write(b'p2p set_sync 12\n')
-	#state = ser.read(10)
+	state = ser.read(10)
+	readback = state.decode('utf-8','ignore')
+	print (readback)
 	ser.write(b'p2p set_freq 926500000\r\n')
-	#state = ser.read(10)
+	state = ser.read(10)
 	ser.write(b'p2p set_sf 7\r\n')
-	#state = ser.read(10)
+	state = ser.read(10)
 	ser.write(b'p2p set_bw 125\r\n')
-	#state = ser.read(10)
+	state = ser.read(10)
 	ser.write(b'p2p tx 1234567890\r\n')
 	state = ser.read(10)
 	readback = state.decode('utf-8','ignore')
