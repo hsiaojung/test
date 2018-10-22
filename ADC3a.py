0bc7003a2570512f83f0399032476ff3b8aa6f44
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-22 14:45:50 +0800

    mi

--- ADC3a.py
+++ ADC3a.py
@@ -9,16 +9,15 @@ i2c_address = 0x4B  # default address
 t = 0
 while True:
 # Reads word (2 bytes) as int
-    rd = bus.read_word_data(i2c_address, 0)
-    block = bus.read_i2c_block_data(i2c_address, 0, 4)
+    #rd = bus.read_word_data(i2c_address, 0)
+    block = bus.read_i2c_block_data(i2c_address, 0, 2)
     print(block)
     # Returned value is a list of 16 bytes
-    print(block)
 # Exchanges high and low bytes
-    data = ((rd & 0xFF) << 8) | ((rd & 0xFF00) >> 8)
+    #data = ((rd & 0xFF) << 8) | ((rd & 0xFF00) >> 8)
 # Ignores two least significiant bits
-    data = data >> 2
-    print("read word date in Dec={%d},in hex={%x},time duration={%2f} "%(rd,rd,t)) 
+    #data = data >> 2
+    #print("read word date in Dec={%d},in hex={%x},time duration={%2f} "%(rd,rd,t)) 
     t += 0.0029
     time.sleep(t)
     
