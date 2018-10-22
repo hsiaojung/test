d99b8fd0582cbc6ade6c4f8d69a1e4267f83d03a
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-22 14:12:43 +0800

    mi

--- ADC3a.py
+++ ADC3a.py
@@ -15,7 +15,7 @@ while True:
     data = ((rd & 0xFF) << 8) | ((rd & 0xFF00) >> 8)
     # Ignores two least significiant bits
     data = data >> 2
-    print("T={%s}"%data) 
+    print("T={%s}"%t) 
 
     t += 0.1
     time.sleep(0.1)
