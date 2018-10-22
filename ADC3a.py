0ec9dc9bd2af425f5318789eb848a1f2141cacac
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-22 17:03:20 +0800

    mi

--- ADC3a.py
+++ ADC3a.py
@@ -20,6 +20,7 @@ while True:
 # Ignores two least significiant bits
     #data = data >> 2
     print("read word date in Dec={%d},in hex={%x},time duration={%2f} "%(rd,rd,t)) 
-    t += 0.001
+    print(datetime.datetime.now())
+    t += 0.0001
     time.sleep(t)
     
