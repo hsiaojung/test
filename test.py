39fe69d0ea47db2c7361a6ba696fed7419741b6b
cyril.chang <cyril_chang@aewin.com.tw>
2018-10-30 18:07:43 +0800

    1

--- test.py
+++ test.py
@@ -413,7 +413,7 @@ def print_menu2():
             print("\n please wait to off line for 4S\n \n")
             print("\n \n")
             break       
-         if case('77'):
+         if case('11'):
             os.system("sudo ethtool -e eth0 ")
             print ("\n")
             for i in range(0x01f0):
