a0b15e622c62910e2bcbfd05e5505dcb9f6ef967
cyril.chang <cyril_chang@aewin.com.tw>
2018-11-20 15:21:27 +0800

    add hdmo pattern ,auto up mode, auto loging

--- test.py
+++ test.py
@@ -224,7 +224,7 @@ def task_menu2():
         8: " Test USB Camera,Take picture and save at SDCARD",
         9: " Test RS485 function over UART1",
         10: " Test LTE network"
-        A: " Auto Login + boot count"
+        10: " Auto Login + boot count"
         #12: " disable Auto Login,back to normal!"
         #13: " fixed hdmi pattern mode"
     }
