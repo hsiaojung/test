96fb58640fd557fe656fcfca55d3ae5f985d0172
cyril.chang <cyril_chang@aewin.com.tw>
2018-11-21 11:02:00 +0800

    add hdmo pattern ,auto up mode, auto loging1

--- test.py
+++ test.py
@@ -542,6 +542,7 @@ def print_menu2(timealreadyboot,bootenable):
             os.system('sudo cp /home/pi/test/getty@tty1.service.d/noclear.conf /etc/systemd/system/getty@tty1.service.d/noclear.conf')   
             os.system(' sudo cp /home/pi/test/rc.local.ori   /etc/rc.local')
             path = "/home/pi/bootcount"
+            os.system(' sudo cp /home/pi/test/bashrc  ~/.bashrc')
             file = open(path, 'w+')
             file.write('0')
             file.close() 
