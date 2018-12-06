26f9de8ffb6a54e13ba9ccc3abab0ff66b3bdc0e
cyril.chang <cyril_chang@aewin.com.tw>
2018-12-06 14:57:39 +0800

    read gpio12..1

--- test.py
+++ test.py
@@ -644,7 +644,8 @@ def print_menu2(timealreadyboot,bootenable):
             import RPi.GPIO as GPIO
             GPIO.setwarnings(False) 
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+            #GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+            GPIO.setup(pin, GPIO.IN)
             print("read GPIO12 =",GPIO.input(pin)) 
             GPIO.cleanup()
 
@@ -652,7 +653,8 @@ def print_menu2(timealreadyboot,bootenable):
             import RPi.GPIO as GPIO
             GPIO.setwarnings(False) 
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+            #GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+            GPIO.setup(pin, GPIO.IN)
             print("read GPIO13 =",GPIO.input(pin)) 
             GPIO.cleanup()
             print("\n\n\n")
