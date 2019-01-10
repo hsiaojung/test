d71cda736345b49670bba62e5beede0fbcbd3347
cyril.chang <cyril_chang@aewin.com.tw>
2019-01-10 14:59:05 +0800

    add test net

--- test.py
+++ test.py
@@ -661,11 +661,8 @@ def print_menu2(timealreadyboot,bootenable):
         if case('17'):
             print('==\n  Test Network(ETH0) by requesting dhcp server to Ping 8.8.8.8==\n\n')
             os.system('sudo dhclient eth0')
-            
-			os.system('sudo dhclient eth0')
-			
+            os.system('sudo dhclient eth0')
 			time.sleep(2)
-			
 			response = os.system("ping -c 1 " + hostname)
 
 			if response == 0:
