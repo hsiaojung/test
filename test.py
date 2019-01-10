af2e1d39083995690d7873d77751e48ce99ec4cd
cyril.chang <cyril_chang@aewin.com.tw>
2019-01-10 14:57:13 +0800

    add test net

--- test.py
+++ test.py
@@ -658,8 +658,7 @@ def print_menu2(timealreadyboot,bootenable):
             print("read GPIO13 =",GPIO.input(pin)) 
             GPIO.cleanup()
             print("\n\n\n")
-            break  
-       if case('17'):
+        if case('17'):
             print('==\n  Test Network(ETH0) by requesting dhcp server to Ping 8.8.8.8==\n\n')
 			os.system('sudo dhclient eth0')  
 			os.system('sudo dhclient eth0')
