659ad3a0176d7897f40432d2511f2108bcc04dfa
cyril.chang <cyril_chang@aewin.com.tw>
2019-01-10 14:59:50 +0800

    add test net

--- test.py
+++ test.py
@@ -662,17 +662,13 @@ def print_menu2(timealreadyboot,bootenable):
             print('==\n  Test Network(ETH0) by requesting dhcp server to Ping 8.8.8.8==\n\n')
             os.system('sudo dhclient eth0')
             os.system('sudo dhclient eth0')
-			time.sleep(2)
-			response = os.system("ping -c 1 " + hostname)
-
+			time.sleep(2)response = os.system("ping -c 1 " + hostname)
 			if response == 0:
 				print ("Network(ETH0) is fine!")
 				
 			else:
 				print ("Network(ETH0) is failed!")
-
-            break  
-                  
+            break      
         if case('99'):
             print('==\n reboot system ! ==\n\n') 
           
