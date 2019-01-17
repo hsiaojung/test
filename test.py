cd72fcfb8f1de41894598da6d8b95c06bf1b345f
cyril.chang <cyril_chang@aewin.com.tw>
2019-01-17 17:20:30 +0800

    add memster..test time to 8 hours

--- test.py
+++ test.py
@@ -817,7 +817,7 @@ def print_menu2(timealreadyboot,bootenable):
         if case('98'):
         
             print('==\n memtester  ! ==\n\n')
-            os.system('sudo memtester 800M 1 |tee /tmp/log')
+            os.system('sudo memtester 800M 17 |tee /tmp/log')
             
             break
                            
